2b9481b6e7107f2ee24b22bba70d87d5eeffcc85
Frances Price-Herndl <fp22b@fsu.edu>
2024-09-14 22:16:31 -0400

    hide and seek w/ arrow keys and mouse

--- sketch.js
+++ sketch.js
@@ -1,8 +1,37 @@
+let theFont;
+var angle = 0;
+var d1;
+
+var textX = 200
+var textY = 200
+
+
+function preload() {
+  theFont = loadFont("assets/ProtestGuerrilla-Regular.ttf");
+}
+
 function setup() {
   createCanvas(400, 400);
+  textFont(theFont);
+  textSize(35);
+  noStroke();
 }
 
 function draw() {
-  background(125);
-  circle(width/2, height/2, 40);
+  background(0);
+  
+  ellipse(mouseX, mouseY, 100, 100);
+  text("Hide", textX, textY, 100, 100);
+
+  if (keyIsPressed){
+    if (keyCode ==  RIGHT_ARROW) {
+      textX +=5;
+    } else if (keyCode == LEFT_ARROW) {
+      textX -= 5;
+    } else if (keyCode == UP_ARROW) {
+      textY -= 5;
+    }else if (keyCode == DOWN_ARROW) {
+      textY +=5;
+    }
+  }
 }
