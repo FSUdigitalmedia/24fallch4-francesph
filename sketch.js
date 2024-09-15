aae9cb30003cdce22c74157258b71a9edec6c900
Frances Price-Herndl <fp22b@fsu.edu>
2024-09-15 13:26:56 -0400

    mouse press = word generates elsewhere

--- sketch.js
+++ sketch.js
@@ -1,51 +1,63 @@
 let theFont;
-let posX;
-let posY;
+
+let textX;
+let textY;
+let textColor;
 
 let distance;
-let threshold = 100
+let threshold = 150
 
-
+//______________________________________________________________
 function preload() {
   theFont = loadFont("assets/ProtestGuerrilla-Regular.ttf");
 }
-
+//______________________________________________________________
 function setup() {
   createCanvas(400, 400);
-  rectMode(CENTER);
 
-  posX = 200;
-  posY = 200;
+  setPositionAndColor();
+
   textFont(theFont);
   textSize(35);
   noStroke();
 }
-
+//___________________________________________________________
+//pt1: when mouse is clicked, word moves
+function setPositionAndColor() {
+  textX = random(0, width);
+  textY = random(0, height);
+  textColor = color(0);
+}
+//_________________________________________________________
 function draw() {
   background(0);
 
+  //flashlight
   fill(255);
   ellipse(mouseX, mouseY, 80, 80);
   
-  fill(0)
-  text("Hide", posX, posY, 100, 100);
+  fill(textColor);
+  text("Hide", textX, textY, 100);
   
-  distance = dist(mouseX, mouseY, posX, posY);
+  //makes word move away from the mouse
+  distance = dist(mouseX, mouseY, textX, textY);
   
   if (distance < threshold) {
-    if (mouseX < posX) {
-      posX = posX + 1;
-    } else if (mouseX > posX) {
-      posX = posX - 1;
+    if (mouseX < textX) {
+      textX = textX + 1;
+    } else if (mouseX > textX) {
+      textX = textX - 1;
     }
     
-    if (mouseY < posY) {
-      posY = posY + 1;
-    } else if (mouseY > posY) {
-      posY = posY - 1;
+    if (mouseY < textY) {
+      textY = textY + 1;
+    } else if (mouseY > textY) {
+      textY = textY - 1;
     }
   }
-
-  
-
+}
+//___________________________________________________________
+//pt2: when mouse is pressed word spawns elsewhere
+function mousePressed() {
+  setPositionAndColor();
 }
