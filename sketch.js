3fe2158eb4d72f89e1bde3989fa25ab3cf36a8a0
Frances Price-Herndl <fp22b@fsu.edu>
2024-09-15 00:07:57 -0400

    word moves away from circle

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 let theFont;
-var angle = 0;
-var d1;
+let posX;
+let posY;
 
-var textX = 200
-var textY = 200
+let distance;
+let threshold = 100
 
 
 function preload() {
@@ -12,26 +12,37 @@ function preload() {
 
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER);
+
+  posX = 200;
+  posY = 200;
   textFont(theFont);
   textSize(35);
   noStroke();
 }
 
 function draw() {
-  background(0);
-  
-  ellipse(mouseX, mouseY, 100, 100);
-  text("Hide", textX, textY, 100, 100);
+  background(110);
 
-  if (keyIsPressed){
-    if (keyCode ==  RIGHT_ARROW) {
-      textX +=5;
-    } else if (keyCode == LEFT_ARROW) {
-      textX -= 5;
-    } else if (keyCode == UP_ARROW) {
-      textY -= 5;
-    }else if (keyCode == DOWN_ARROW) {
-      textY +=5;
+  fill(255);
+  ellipse(mouseX, mouseY, 80, 80);
+  
+  fill(0)
+  text("Hide", posX, posY, 100, 100);
+  
+  distance = dist(mouseX, mouseY, posX, posY);
+  
+  if (distance < threshold) {
+    if (mouseX < posX) {
+      posX = posX + 1;
+    } else if (mouseX > posX) {
+      posX = posX - 1;
+    }
+    
+    if (mouseY < posY) {
+      posY = posY + 1;
+    } else if (mouseY > posY) {
+      posY = posY - 1;
     }
   }
 }
